a4d3ec1c76adf5af487bf06eaf48c7bc0b17c659
Devon DeJohn <ddejohn@msudenver.edu>
2020-02-28 23:33:02 -0700

    pylint bs

--- prg01.py
+++ prg01.py
@@ -1,8 +1,12 @@
+"""Lexical and syntax analyzer (LR parser) for a C-lite language,
+written for CS-3210 Principles of Programming Languages by Devon DeJohn"""
+
 from prg01_data import token_data as tkd
 
 
 class Lexeme:
-    def __init__(self, label:str):
+    """Lexeme object which holds its label and corresponding token"""
+    def __init__(self, label: str):
         self.label = label
         self.token = tkd.lookup(self.label)
         if not self.token:
@@ -11,24 +15,31 @@ class Lexeme:
 
 
 class Tree:
+    """Generic tree-like structure for generating the parse tree"""
     def __init__(self):
         self.branch = None
         self.leaf = Lexeme
+
+    def sprout(self, new_branch: Tree):
+        """Add a subtree"""
+        self.branch = new_branch
 # end
 
 
 class Source:
-    def __init__(self, source:str):
+    """Source object which holds the source file, lexemes, and parse tree"""
+    def __init__(self, source: str):
         self.source = source
         self.lexemes = []
         self.parse_tree = Tree
 
     def lexer(self):
+        """Split source at whitespace, instatiate lexeme, add to list"""
         proto_lexemes = self.source.split()
         for lex in proto_lexemes:
             new_lex = Lexeme(lex)
             self.lexemes.append(new_lex)
 
     def parser(self):
-        pass
+        """Basic LR parser (shift-reduce)"""
 # end
