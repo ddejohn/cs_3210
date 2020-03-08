8cd1954a0d24bb746737e24bd91f9c9af1b12368
ddejohn <ddejohn@pm.me>
2020-03-07 20:35:50 -0700

    docstrings

--- parser/utilities/grammar_reader.py
+++ parser/utilities/grammar_reader.py
@@ -2,8 +2,7 @@ import csv
 
 
 def get_slr(path):
-    """ Read a grammar table as csv, return
-        separate ACTION and GOTO 2D arrays """
+    """Read a grammar table as csv, return separate ACTION and GOTO tables"""
     with open(path) as g:
         reader = csv.reader(g)
         next(reader)
@@ -15,16 +14,14 @@ def get_slr(path):
 
 
 def get_productions(path):
-    """ Parse a production list for the grammar
-        at 'path' """
+    """Parse a production list for the grammar at 'path'"""
     with open(path) as p:
         productions = p.read()
     
 
 
 def _col_split(mat, col):
-    """ Split a 2D array into a left 2D array
-        and a right 2D array at column 'col' """
+    """Split a 2D array into left and right 2D arrays at column 'col'"""
     left_mat = []
     right_mat = []
     for row in mat:
